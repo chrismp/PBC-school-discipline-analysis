4726de879f9086a024635441fc7d3af09ee4e69d
Chris Persaud <chris@homelab.homelab.com>
2018-09-16 21:36:44 -0400

    Lots of styling to scatterplot

--- script.r
+++ script.r
@@ -1,6 +1,12 @@
 # install.packages("dplyr")
+# install.packages("ggplot2")
+# install.packages('ggthemes', dependencies = TRUE)
+# install.packages("showtext", dependencies = T)
 
 require(dplyr)
+require(ggplot2)
+require(ggthemes)
+require(showtext)
 
 
 ## FUNCTIONS
@@ -41,6 +47,8 @@ func.makePunishmentRateData <- function(dat){
   dat.output$PunishmentRate <- dat.output$Punished / dat.output$Enrollment
   return(dat.output)
 }
+
+func.percentFormat <- function(x) paste0(x*100,'%')
 
 
 ## ENROLLMENT
@@ -151,18 +159,121 @@ func.makePunishmentRateData <- function(dat){
   dat.output.punishmentRates.pbc <- func.makePunishmentRateData(dat.merge.pbc)
 
 
-# WRITE TO CSVs
-  outputDir <- "output/"
-  write.csv(
-    x = dat.output.punishmentRates.state,
-    file = paste0(outputDir,"Punishment and enrollment by race - state.csv"),
-    row.names = F
+# MAKE CHARTS
+  # Pre-styling: Common styles for charts
+  showtext_auto()
+  hedFont <- "News Cycle" # Or 'Pragati Narrow'
+  font_add_google(
+    name = hedFont,
+    family = hedFont,
+    regular.wt = 400,
+    bold.wt = 700
   )
-  write.csv(
-    x = dat.output.punishmentRates.pbc,
-    file = paste0(outputDir,"Punishment and enrollment by race - PBC.csv"),
-    row.names = F
+  
+  chartStyle.backgroundColor <- "#eeefff"
+  chartStyle.lineColor <- "#cccccc"
+  chartStyle.trendLineThickness <- 0.5
+  chartStyle.scatterplot.dotSize <- 3
+  chartStyle.scatterplot.alpha <- 0.75
+  
+  chartStyle.text <- theme(
+    plot.title = element_text(
+      size = 18,
+      family = hedFont,
+      face = "bold",
+      hjust = 0.5
+    ),
+    plot.subtitle = element_text(
+      size = 14,
+      hjust = -1.19
+    ),
+    plot.caption = element_text(
+      size = 10
+    ),
+    axis.title = element_text(
+      face = "bold"
+    )
   )
+  
+  chartStyle.misc <- theme(
+    plot.background = element_rect(
+      fill = chartStyle.backgroundColor
+    ),
+    axis.ticks = element_blank(),
+    panel.background = element_rect(
+      fill = chartStyle.backgroundColor
+    ),
+    panel.grid.major = element_line(
+      color = chartStyle.lineColor,
+      size = 0.25
+    ),
+    panel.grid.minor = element_blank(),
+    axis.line.x = element_line(
+      color = "#000000"
+    )
+  )
+  
+  # Scatterplot: % white students punished vs % black students punished, by school in PBC
+  ggplot(
+    data = dat.merge.pbc,
+    aes(
+      x = PercentOfWhiteStudentsPunished,
+      y = PercentOfBlackStudentsPunished
+    )
+  ) + 
+    geom_point(
+      size = chartStyle.scatterplot.dotSize,
+      alpha = chartStyle.scatterplot.alpha,
+      color = "#ff8a59"
+    ) +
+    geom_smooth(
+      method = "lm",
+      se = F,
+      size = chartStyle.trendLineThickness,
+      color = "black"
+    ) + 
+    scale_x_continuous(
+      name = "Percent of white students punished",
+      labels = func.percentFormat,
+      breaks = seq(
+        from = min(dat.merge.pbc$PercentOfWhiteStudentsPunished), 
+        to = max(dat.merge.pbc$PercentOfWhiteStudentsPunished),
+        by = 0.05
+      )
+    ) +
+    scale_y_continuous(
+      name = "Percent of black students punished",
+      labels = func.percentFormat,
+      expand = c(0,0),
+      breaks = seq(
+        from = min(dat.merge.pbc$PercentOfBlackStudentsPunished), 
+        to = max(dat.merge.pbc$PercentOfBlackStudentsPunished),
+        by = 0.1
+      )
+    ) +
+    labs(
+      title = "Punishment by race in Palm Beach County, Fla. schools",
+      subtitle = "Punishment rates for black students vs. white students, 2016-17 school year",
+      caption = "Source: Florida Dept. of Education"
+    ) + 
+    coord_cartesian(clip = "off") +
+    chartStyle.text +
+    chartStyle.misc
+  
+  
+  
+# # WRITE TO CSVs
+#   outputDir <- "output/"
+#   write.csv(
+#     x = dat.output.punishmentRates.state,
+#     file = paste0(outputDir,"Punishment and enrollment by race - state.csv"),
+#     row.names = F
+#   )
+#   write.csv(
+#     x = dat.output.punishmentRates.pbc,
+#     file = paste0(outputDir,"Punishment and enrollment by race - PBC.csv"),
+#     row.names = F
+#   )
   
   
   
